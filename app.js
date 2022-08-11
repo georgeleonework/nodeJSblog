780870a4217470ed1141b99c83e741e3d14039d3
georgeleonework <georgeleonework@gmail.com>
2022-08-11 11:03:57 -0400

    included the create ejs page

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const app = express();
 
 //register view engine //below configures some application settings
 app.set('view engine', 'ejs');
-app.set('views', 'myviews');
+
 //listen for requests
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname });
-}); //basic setup here to respond to a given url with express
+    res.render('index');
+}); 
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
+
+app.get('/blogs/create', (req, res) => {
+    res.render()
+})
 
 app.get('/about-us', (req, res) => {
     res.redirect('/about'); //super easy way to redirect an about-us request to about page
@@ -23,5 +27,5 @@ app.get('/about-us', (req, res) => {
 //if there is still no match at this point for the request, we will "use" the following function
 ///this is to create middleware and use middleware functions in express
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 })
