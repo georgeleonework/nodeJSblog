7da10c37ead1f74394be4d2a1a5828f2ab10e6d3
georgeleonework <georgeleonework@gmail.com>
2022-08-11 10:37:48 -0400

    added view engine ejs to app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+//register view engine //below configures some application settings
+app.set('view engine', 'ejs');
+app.set('views', 'myviews');
 //listen for requests
 app.listen(3000)
 
@@ -15,4 +18,10 @@ app.get('/about', (req, res) => {
 
 app.get('/about-us', (req, res) => {
     res.redirect('/about'); //super easy way to redirect an about-us request to about page
+})
+
+//if there is still no match at this point for the request, we will "use" the following function
+///this is to create middleware and use middleware functions in express
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
 })
