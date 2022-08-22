fbb007e380d3d09cbd1223ffc5a82e0f67eab6c6
georgeleonework <georgeleonework@gmail.com>
2022-08-22 15:45:50 -0400

    finished draft of blog including MVC through nodejs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,11 +1,9 @@
-//creating the following functions
-// blog_index, blog_details, blog_create_get, blog_create_post, blog_delete
 const Blog = require('../models/blog');
 
 const blog_index = (req, res) => {
     Blog.find().sort({ createdAt: -1 })
       .then(result => {
-        res.render('index', { blogs: result, title: 'All blogs' });
+        res.render('blogs/index', { blogs: result, title: 'All blogs' });
       })
       .catch(err => {
         console.log(err);
@@ -16,7 +14,7 @@ const blog_details = (req, res) => {
     const id = req.params.id; //we need to first extract the id parameter from the request
     Blog.findById(id)
         .then(result => {
-            res.render('details', { blog: result })
+            res.render('blogs/details', { blog: result })
         })
         .catch(err => {
             console.log(err);
@@ -24,11 +22,36 @@ const blog_details = (req, res) => {
 }
 
 const blog_create_get = (req, res) => {
-    res.render('create', {title: 'Create a new Blog'});
+    res.render('blogs/create', {title: 'Create a new Blog'});
+}
+
+const blog_create_post = (req, res) => {
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+const blog_delete = (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs'})
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
 module.exports = {
     blog_index,
     blog_details,
-    blog_create_get
+    blog_create_get,
+    blog_create_post,
+    blog_delete
 } //now we have access to this controller from anywhere that we import the file
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,38 +5,10 @@ const blogController = require('../controllers/blogController')
 const router = express.Router();
 
 router.get('/', blogController.blog_index);
-
-
-//the follow block of code takes anything submitted to the create form and save it to the database
-router.post('/', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
+router.post('/', blogController.blog_create_post);
 router.get('/create', blogController.blog_create_get);
-
-
 router.get('/:id', blogController.blog_details);
-    
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs'})
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
+router.delete('/:id', blogController.blog_delete);
 
 
 module.exports = router;
